c6db724a26155d01e52dbbd167991acb7eb8af70
Priyansh <doodle.ui1210@gmail.com>
2026-04-20 18:42:24 +0530

    feat: redesign slash popup with rounded items, hover states, and slide animation

--- src/Antinote/Views/SlashCommandPopup.xaml.cs
+++ src/Antinote/Views/SlashCommandPopup.xaml.cs
@@ -1,5 +1,7 @@
-using System.Windows.Controls;
+using System.Windows;
 using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 using Antinote.Commands;
 
 namespace Antinote.Views;
@@ -18,7 +20,11 @@ public partial class SlashCommandPopup : Popup
         CommandList.ItemsSource = commands;
         if (commands.Count > 0)
             CommandList.SelectedIndex = 0;
-        IsOpen = commands.Count > 0;
+
+        if (commands.Count > 0 && !base.IsOpen)
+            AnimateOpen();
+        else if (commands.Count == 0 && base.IsOpen)
+            AnimateClose();
     }
 
     public void MoveSelectionUp()
@@ -36,10 +42,37 @@ public partial class SlashCommandPopup : Popup
     public void ConfirmSelection()
     {
         if (CommandList.SelectedItem is SlashCommand cmd)
+            AnimateClose(() => CommandSelected?.Invoke(cmd));
+    }
+
+    public new bool IsOpen
+    {
+        get => base.IsOpen;
+        set { if (!value && base.IsOpen) AnimateClose(); else base.IsOpen = value; }
+    }
+
+    private void AnimateOpen()
+    {
+        base.IsOpen = true;
+        var duration = new Duration(TimeSpan.FromMilliseconds(120));
+        var ease = new CubicEase { EasingMode = EasingMode.EaseOut };
+
+        PopupBorder.BeginAnimation(UIElement.OpacityProperty, new DoubleAnimation(0, 1, duration) { EasingFunction = ease });
+        SlideTransform.BeginAnimation(TranslateTransform.YProperty, new DoubleAnimation(8, 0, duration) { EasingFunction = ease });
+    }
+
+    private void AnimateClose(Action? onComplete = null)
+    {
+        var duration = new Duration(TimeSpan.FromMilliseconds(80));
+        var opacityAnim = new DoubleAnimation(1, 0, duration);
+        opacityAnim.Completed += (_, _) =>
         {
-            IsOpen = false;
-            CommandSelected?.Invoke(cmd);
-        }
+            base.IsOpen = false;
+            onComplete?.Invoke();
+        };
+
+        PopupBorder.BeginAnimation(UIElement.OpacityProperty, opacityAnim);
+        SlideTransform.BeginAnimation(TranslateTransform.YProperty, new DoubleAnimation(0, 8, duration));
     }
 
     private void CommandList_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
