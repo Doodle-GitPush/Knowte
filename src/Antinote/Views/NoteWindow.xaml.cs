4a1d6d599a5ef1522c2751f3c585230024481e47
Priyansh <doodle.ui1210@gmail.com>
2026-04-20 17:46:27 +0530

    feat: wire slash command engine, popup, and math evaluation into NoteWindow

--- src/Antinote/Views/NoteWindow.xaml.cs
+++ src/Antinote/Views/NoteWindow.xaml.cs
@@ -2,6 +2,7 @@ using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Animation;
 using System.Windows.Threading;
+using Antinote.Commands;
 using Antinote.Rendering;
 using Antinote.Storage;
 
@@ -11,6 +12,9 @@ public partial class NoteWindow : Window
 {
     private readonly NoteStorage _storage;
     private readonly DispatcherTimer _saveTimer;
+    private readonly SlashCommandEngine _slashEngine;
+    private readonly SlashCommandPopup _slashPopup;
+    private bool _suppressTextChanged;
 
     public NoteWindow(NoteStorage storage)
     {
@@ -22,9 +26,25 @@ public partial class NoteWindow : Window
         _saveTimer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(500) };
         _saveTimer.Tick += (_, _) => { _saveTimer.Stop(); SaveNote(); };
 
+        var registry = new SlashCommandRegistry();
+        var mathEval = new MathEvaluator();
+        _slashEngine = new SlashCommandEngine(Editor, registry, mathEval);
+        _slashEngine.SuggestionsChanged += OnSuggestionsChanged;
+        _slashEngine.SuggestionsDismissed += OnSuggestionsDismissed;
+
+        _slashPopup = new SlashCommandPopup();
+        _slashPopup.PlacementTarget = Editor;
+        _slashPopup.CommandSelected += cmd =>
+        {
+            _suppressTextChanged = true;
+            _slashEngine.ApplyCommand(cmd);
+            _suppressTextChanged = false;
+        };
+
         DateLabel.Text = DateTime.Today.ToString("dddd, MMMM d");
 
         Editor.TextChanged += Editor_TextChanged;
+        Editor.TextArea.PreviewKeyDown += Editor_PreviewKeyDown;
 
         Deactivated += (_, _) => Hide();
         KeyDown += (_, e) => { if (e.Key == Key.Escape) Hide(); };
@@ -43,17 +63,62 @@ public partial class NoteWindow : Window
 
     public new void Hide()
     {
+        _slashPopup.IsOpen = false;
         FadeOut(() => base.Hide());
     }
 
-    private void SaveNote() => _storage.Save(Editor.Text);
+    private void Editor_PreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        if (_slashPopup.IsOpen)
+        {
+            switch (e.Key)
+            {
+                case Key.Up:
+                    _slashPopup.MoveSelectionUp();
+                    e.Handled = true;
+                    return;
+                case Key.Down:
+                    _slashPopup.MoveSelectionDown();
+                    e.Handled = true;
+                    return;
+                case Key.Enter:
+                case Key.Tab:
+                    _slashPopup.ConfirmSelection();
+                    e.Handled = true;
+                    return;
+                case Key.Escape:
+                    _slashPopup.IsOpen = false;
+                    e.Handled = true;
+                    return;
+            }
+        }
+
+        if (e.Key == Key.Enter && !_slashPopup.IsOpen)
+        {
+            _slashEngine.HandleEnterKey();
+            e.Handled = true;
+        }
+    }
 
     private void Editor_TextChanged(object? sender, EventArgs e)
     {
+        if (_suppressTextChanged) return;
         UpdatePlaceholder();
+        _slashEngine.HandleTextChanged();
         _saveTimer.Stop();
         _saveTimer.Start();
     }
+
+    private void OnSuggestionsChanged(List<SlashCommand> commands, (double X, double Y) pos)
+    {
+        _slashPopup.HorizontalOffset = pos.X;
+        _slashPopup.VerticalOffset = pos.Y + 4;
+        _slashPopup.UpdateCommands(commands);
+    }
+
+    private void OnSuggestionsDismissed() => _slashPopup.IsOpen = false;
+
+    private void SaveNote() => _storage.Save(Editor.Text);
 
     private void UpdatePlaceholder() =>
         Placeholder.Visibility = string.IsNullOrWhiteSpace(Editor.Text)
