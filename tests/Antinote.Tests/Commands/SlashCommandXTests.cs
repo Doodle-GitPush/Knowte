1cb1c682d19dc5029300f513ccb3f3fb8ddd2ffa
Priyansh <doodle.ui1210@gmail.com>
2026-04-20 18:41:19 +0530

    feat: add /x command to toggle checklist items

---
+++ tests/Antinote.Tests/Commands/SlashCommandXTests.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using Antinote.Commands;
+
+namespace Antinote.Tests.Commands;
+
+[TestFixture]
+public class SlashCommandXTests
+{
+    [Test]
+    public void ToggleChecklistLine_Unchecked_BecomesChecked()
+    {
+        var result = SlashCommandEngine.ToggleChecklistOnLine("- [ ] Buy milk");
+        Assert.That(result, Is.EqualTo("- [x] Buy milk"));
+    }
+
+    [Test]
+    public void ToggleChecklistLine_Checked_BecomesUnchecked()
+    {
+        var result = SlashCommandEngine.ToggleChecklistOnLine("- [x] Buy milk");
+        Assert.That(result, Is.EqualTo("- [ ] Buy milk"));
+    }
+
+    [Test]
+    public void ToggleChecklistLine_NoCheckbox_ReturnsNull()
+    {
+        var result = SlashCommandEngine.ToggleChecklistOnLine("just a note");
+        Assert.That(result, Is.Null);
+    }
+}
